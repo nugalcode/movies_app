00880347cf398cf1e95d39023923ea2b99287182
Jed Nugal <nugaljed@gmail.com>
2022-02-15 15:13:44 -0800

    Changing color of navbar links depending on which page we're on
    I did this so that it's clear to the user which page they're on. I used useState along with className and onClick to determine which link to be colored.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 
 const Navbar = () => {
+
+    const [home, setHome] = useState(true);
+    const [favorites, setFavorites] = useState(false);
+
+
+    const handleOnClick = (page) => {
+        if (page === "home") {
+            if (home)
+                return;
+            else {
+                setHome(true)
+                setFavorites(false);
+            }
+        }
+        else {
+            if (favorites)
+                return;
+            else {
+                setFavorites(true);
+                setHome(false);
+            }
+        }
+    }
+
     return (
         <div className="nav">
             <li>
-                <Link to="/"> Home </Link>
+                <Link
+                    to="/"
+                    className={home ? "navLink currPage" : "navLink"}
+                    onClick={() => handleOnClick("home")}
+                >
+                    Home
+                </Link>
             </li>
+
             <li>
-                <Link to="/Favorites"> Favorites </Link>
+                <Link
+                    to="/Favorites"
+                    className={favorites ? "navLink currPage" : "navLink"}
+                    onClick={() => handleOnClick("favorites")}
+                >
+                    Favorites
+                </Link>
             </li>
         </div>
     );
